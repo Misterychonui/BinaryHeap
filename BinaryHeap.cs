ef5f6c2432baf91edeaa30fd7203665074c802ad
Misterychonui <m-sych@inbox.ru>
2025-06-29 22:36:08 +0300

    Добавьте файлы проекта.

---
+++ BinaryHeap.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Binaryheap<T> where T : IComparable<T>
+{
+    private readonly List<T> _heap;
+
+    public Binaryheap()
+    {
+        _heap = new List<T>();
+    }
+    public int Count()
+    {
+        return _heap.Count;
+    }
+    public void Insert(T value)
+    {
+        _heap.Add(value);
+
+        int i = _heap.Count - 1;
+        int parent = (i - 1) / 2;
+
+        while (i > 0 && _heap[parent].CompareTo(_heap[i])>0)
+        {
+            T temp = _heap[i];
+            _heap[i] = _heap[parent];
+            _heap[parent] = temp;
+
+            i = parent;
+            parent = (i - 1) / 2;
+        }
+    }
+    public T FindMax()
+    {
+        return _heap[0];
+    }
+    public T FindNextElement(int index)
+    {
+        return _heap[index];
+    }
+    public T DeleteElement() 
+    {
+        T temp = _heap[0];
+        _heap[0] = _heap[_heap.Count - 1];
+        _heap.RemoveAt(_heap.Count - 1);
+        Heapify(0);
+        return temp;
+    }
+    private void Heapify(int i)
+    {
+        int leftChild;
+        int rightChild;
+        int minChild;
+
+        for (; ; )
+        {
+            leftChild = 2 * i + 1;
+            rightChild = 2 * i + 2;
+            minChild = i;
+
+            if (leftChild < _heap.Count && _heap[leftChild].CompareTo(_heap[minChild])<0)
+            {
+                minChild = leftChild;
+            }
+
+            if (rightChild < _heap.Count && _heap[rightChild].CompareTo(_heap[minChild])<0)
+            {
+                minChild = rightChild;
+            }
+
+            if (minChild == i)
+            {
+                break;
+            }
+            T temp = _heap[i];
+            _heap[i] = _heap[minChild];
+            _heap[minChild] = temp;
+            i = minChild;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace BinaryHeap
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double[] weights = new double[n];
+            Random r = new Random();
+            for(int i=0; i<n; i++)
+                weights[i] = r.NextDouble();
+            Binaryheap<double> heap = new Binaryheap<double>();
+            double containers = CalculateContainers(heap,weights);
+            Console.WriteLine("Размер кучи: {0}",containers);
+            Console.WriteLine("&&&&&&&&&&&&");
+            Console.WriteLine("Начальные значения");
+            foreach(var t in weights)
+            {
+                Console.WriteLine(t);
+            }
+            Console.WriteLine("&&&&&&&&&&&&");
+            Console.WriteLine("Куча");
+            for(int i=0;i<heap.Count();i++)
+            {
+                Console.WriteLine(heap.FindNextElement(i));
+            }
+        }
+
+        private static double CalculateContainers(Binaryheap<double> heap,double[] weights)
+        {        
+            int containers = 0;
+            double x = 1;
+            for (int i=0;i<weights.Length;i++)
+            {
+                x = 1;
+                if (weights[i] == 1)
+                {
+                    containers++;
+                }
+                else
+                {
+                    if (heap.Count() == 0)
+                    {
+                        heap.Insert(weights[i]);
+                    }
+                    else
+                    {
+                        if(x > heap.FindMax()+weights[i])
+                        {
+                            x = heap.FindMax() + weights[i];
+                            heap.DeleteElement();
+                            heap.Insert(x);
+                        }
+                        else 
+                        {
+                            heap.Insert(weights[i]);
+                        } 
+                    }
+                }
+            }
+            int g = heap.Count();
+            return containers + g;
+        }
+    }
+}
